c74c600011c169560525f6b49ac39278b0e48f43
jnappy <jpnappy@gmail.com>
2015-09-19 16:43:56 -0400

    some initial instrcutions for repeater

---
+++ repeater.rb
@@ -0,0 +1,15 @@
+require "pry"
+
+#write a method that accepts two paramters:
+  # a string to be repeatedly put to the string
+  # how many times that string should be repeated
+
+# ask the user for a string
+
+# store the users input in a variable
+
+# ask the user for the number of times to repeat the string
+
+# store the users input in a variable
+
+# call the method 
